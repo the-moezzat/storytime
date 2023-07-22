a27f8b3d50b97aebc4a2e8e5eee395dcab4f62e0
the-moezzat <dev.moezzat@gmail.com>
2023-07-22 14:19:53 +0300

    Dummy: this is dummy commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
 import Registration from "./pages/Registration";
 import Login from "./features/registration/Login";
 import SignUp from "./features/registration/SignUp";
+import { Button } from "./ui/button";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to={"registration/login"} />} />
+        <Route
+          path="/"
+          element={
+            <Button>
+              <Link to={"registration/login"}>Login</Link>
+            </Button>
+          }
+        />
         <Route path="registration" element={<Registration />}>
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<SignUp />} />
