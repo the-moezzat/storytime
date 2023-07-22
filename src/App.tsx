e3ed25a88ddc47701c4381acbe46b01e0a4600b4
the-moezzat <dev.moezzat@gmail.com>
2023-07-22 14:01:55 +0300

    fix: debuging the proplem

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Registration from "./pages/Registration";
 import Login from "./features/registration/Login";
-// import SignUp from "./features/registration/SignUp";
+import SignUp from "./features/registration/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Navigate to={"registration/login"} />} />
         <Route path="registration" element={<Registration />}>
           <Route path="login" element={<Login />} />
-          {/* <Route path="signup" element={<SignUp />} /> */}
+          <Route path="signup" element={<SignUp />} />
+          Route
         </Route>
       </Routes>
     </BrowserRouter>
