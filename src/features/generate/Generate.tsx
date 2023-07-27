86516967fb4753d8214f2ac922e236b4fe80d7e6
the-moezzat <dev.moezzat@gmail.com>
2023-07-27 20:15:17 +0300

    test

--- src/features/generate/Generate.tsx
+++ src/features/generate/Generate.tsx
@@ -3,18 +3,44 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import GenerateForm from "./GenerateForm";
 import EbupReader from "./EbupReader";
+import { Button } from "../../ui/button";
+import { useMutation } from "react-query";
+import axios from "axios";
 
 function Generate() {
+  const { mutate, data, isLoading } = useMutation(() =>
+    axios.post("http://104.43.137.129:8000/docs", {
+      prompt:
+        "Alex lives in Paris in 2050, where the effects of global warming are making it difficult to find food and water.",
+      writingStyle:
+        "Clear and easily understandable, similar to a young adult novel. Highly descriptive and sometimes long-winded. Similar to the pulse-pounding intensity of J. R. R. Tolkien, or Stephen King or Agatha Christie.",
+      num_chapters: 1,
+    }),
+  );
+
+  function handleGenerate() {
+    console.log("Generating");
+    mutate();
+    console.log("Done");
+  }
+
+  console.log(isLoading ? "loading" : data);
+
   return (
-    <div className="grid h-full grid-cols-[repeat(24,1fr)] gap-4">
-      <div className="col-span-7 rounded-xl bg-white p-4">
-        <GenerateForm />
+    <>
+      <Button onClick={handleGenerate} disabled={isLoading}>
+        {isLoading ? "Loading..." : "Generate"}
+      </Button>
+      <div className="grid h-full grid-cols-[repeat(24,1fr)] gap-4">
+        <div className="col-span-7 rounded-xl bg-white p-4">
+          <GenerateForm />B
+        </div>
+        <div className="col-[8_/_span_17] rounded-xl bg-white">
+          {/* <div className="text-base">{data}</div> */}
+          <EbupReader />
+        </div>
       </div>
-      <div className="col-[8_/_span_17] rounded-xl bg-white">
-        {/* <div className="text-base">{data}</div> */}
-        <EbupReader />
-      </div>
-    </div>
+    </>
   );
 }
 
--- src/features/generate/GenerateForm.tsx
+++ src/features/generate/GenerateForm.tsx
@@ -33,27 +33,6 @@ import { Switch } from "../../ui/switch";
 import useGenerate from "../../hooks/useGenerate";
 import useGenerateForm from "./useGenerateForm";
 
-// const formSchema = z.object({
-//   description: z.string().min(60, {
-//     message: "Description must be at least 60 characters long",
-//   }),
-//   numberOfChapters: z
-//     .string()
-//     .min(1, {
-//       message: "Number of chapters must be at least 1",
-//     })
-//     .max(2, {
-//       message: "Number of chapters must be at most 13",
-//     }),
-//   title: z.string().min(6),
-//   pointOfView: z.enum(["first", "second", "third"], {
-//     required_error: "You need to select a point of view.",
-//   }),
-//   tone: z.string({
-//     required_error: "Please select an tone.",
-//   }),
-// });
-
 function GenerateForm() {
   const [isDisable, setIsDisable] = useState(true);
   const { generate, story, isLoading } = useGenerate();
