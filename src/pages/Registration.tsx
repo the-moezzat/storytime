70504dd9d11489674b36d93fd614f9f9bd060d05
the-moezzat <dev.moezzat@gmail.com>
2023-09-11 04:06:30 +0300

    refactor: change logo

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -4,7 +4,7 @@ export default function Registration() {
   return (
     <div className=" container mx-auto grid h-screen  grid-cols-12 gap-4 px-4 max-md:grid-cols-1">
       <div className="col-span-6 grid grid-rows-[auto,1fr] py-4">
-        <img src={"/logo.svg"} alt="logo" className="h-10" />
+        <img src={"/logo-black.svg"} alt="logo" className="h-8" />
         <div className=" w- self-center justify-self-center">
           <Outlet />
         </div>
